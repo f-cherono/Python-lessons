069a4635c79f675f946e200e5eaabb13af9509bc
f-cherono <fcherono222@gmail.com>
2024-07-04 14:44:27 +0300

    list

---
+++ lesson3.py
@@ -0,0 +1,17 @@
+cars = ['Honda',67,'{"blue,"red"}','nissan', 'surf','mazda','volkswagen']
+# age = {78,34,25,56}
+print(cars)
+
+# print
+print(cars[3])
+# slicing
+print(cars[:5])
+
+cars.append("BMW")
+print(cars)
+
+cars[0] = 'hondar'
+print(cars)
+
+cars.insert(2,"Toyota")
+print(cars)
---
+++ lesson3a.py
@@ -0,0 +1,18 @@
+fruits = ['avocado','apple','kiwi','watermelon','passionfruit','banana','oranges','pawpaw']
+# date = {06,12,14,18,22,24,28,30}
+print
+print(fruits)
+
+print (fruits[4])
+
+fruits.append("carrot")
+print(fruits)
+
+fruits.pop()
+print(fruits)
+
+fruits.sort()
+print(fruits)
+
+fruits.insert(3,"eggplant")
+print(fruits)
